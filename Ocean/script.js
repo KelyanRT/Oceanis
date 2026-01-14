ee3f928ce1c70939f1c285d5c5bba6ffad0d07ac
KelyanRT <reatekelyan@gmail.com>
2026-01-14 14:57:53 +0100

    Delete Ocean directory

--- Ocean/script.js
+++
@@ -1,8 +0,0 @@
-const burger = document.querySelector('.burger');
-const menu = document.querySelector('nav ul');
-const header = document.querySelector('header');
-
-burger.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    header.classList.toggle('expanded');
-});
